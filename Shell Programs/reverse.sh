69ae01b4a42af38643c6ba2657eb65fecdd17747
ammielpeters <95863004+ammielpeters@users.noreply.github.com>
2025-09-02 22:41:41 +0530

    Create reverse.sh

---
+++ Shell Programs/reverse.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Program to reverse a string
+
+echo "Enter a string: "
+read str
+rev=""
+
+len=${#str}
+for (( i=$len-1; i>=0; i-- ))
+do
+    rev="$rev${str:$i:1}"
+done
+
+echo "Reversed string: $rev"
