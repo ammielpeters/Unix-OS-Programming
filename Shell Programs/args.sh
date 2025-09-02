fca9e9f17c7b8c07a0e73e606cf68b563a8ca540
ammielpeters <95863004+ammielpeters@users.noreply.github.com>
2025-09-02 22:41:06 +0530

    Create args.sh

---
+++ Shell Programs/args.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Program to demonstrate handling of command line arguments
+
+echo "Number of arguments passed: $#"
+echo "All arguments: $@"
+echo "First argument: $1"
+echo "Second argument: $2"
